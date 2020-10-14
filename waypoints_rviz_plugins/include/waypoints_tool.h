eeae8d6f773e20716bcf96f07a2561c1767a078f
Booney <kakathebest@163.com>
2020-10-15 01:18:05 +0800

    added waypoints.tool.h

--- waypoints_rviz_plugins/include/waypoints_tool.h
+++ waypoints_rviz_plugins/include/waypoints_tool.h
@@ -2,9 +2,44 @@
 
 
 #include <rviz/tool.h>
-namespace Follow_Waypoints
-{
-    
 
+
+namespace Waypoints_Plugin {
+    
+using namespace Waypoints_Plugin;
+namespace Ogre
+{
+class SceneNode;
+class Vector3;
+}
+
+namespace rviz
+{
+class VectorProperty;
+class VisualizationManager;
+class ViewportMouseEvent;
+}
+    
+class StarTool: public ::rviz::Tool
+{
+Q_OBJECT
+public:
+  StarTool();
+  ~StarTool();
+
+  virtual void onInitialize();
+
+  virtual void activate();
+  virtual void deactivate();
+
+  virtual int processMouseEvent( rviz::ViewportMouseEvent& event );
+
+  virtual void load( const ::rviz::Config& config );
+  virtual void save( ::rviz::Config config ) const;
+
+private:
+    
+  std::vector<Ogre::SceneNode*> flag_nodes_;
+};
 
 }
---
+++ waypoints_rviz_plugins/src/waypoints_rviz_plugins.cc
@@ -0,0 +1,6 @@
+#include <waypoints_tool.h>
+
+int main(){
+
+    return 0;
+}
